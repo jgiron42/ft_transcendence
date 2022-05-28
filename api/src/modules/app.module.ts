38d5e6b883f2fa8345330916ca48c4806e398ace
riblanc <richard.blc25@gmail.com>
2022-05-28 18:03:34 +0000

    feature(server-chat): Websockets / chat base added.

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,9 +1,10 @@
 import { AppController } from "@controllers/app.controller";
 import { Module } from "@nestjs/common";
 import { AuthModule } from "@modules/auth.module";
+import { ChatModule } from "@modules/chat.module";
 
 @Module({
-	imports: [AuthModule],
+	imports: [AuthModule, ChatModule],
 	controllers: [AppController],
 })
 export class AppModule {}
