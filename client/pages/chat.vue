f228b8c784235dcee815c9e82276b89ef415a3ca
riblanc <richard.blc25@gmail.com>
2022-05-28 20:15:20 +0000

    fix(eslint-client)

--- client/pages/chat.vue
+++ client/pages/chat.vue
@@ -1,14 +1,15 @@
 <template>
-    <div id="app" class="w-screen h-screen overflow-hidden flex flex-col">
-		<Header />	
+	<div id="app" class="w-screen h-screen overflow-hidden flex flex-col">
+		<Header />
 		<div id="page" class="flex flex-col items-center flex-grow min-h-0">
-			<br>
-			<div id="container-test" class="flex flex-row justify-between items-center overflow-y-scroll pb-[15px] gap-10 ">
-				<Chat /> 
+			<div
+				id="container-test"
+				class="flex flex-row justify-between items-center overflow-y-scroll pb-[15px] gap-10"
+			>
+				<Chat />
 			</div>
 		</div>
-    </div>
-
+	</div>
 </template>
 
 <style>
