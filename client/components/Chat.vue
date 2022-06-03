5ac922e3e7fe73fa36ed91ff393fb432b98e337d
riblanc <richard.blc25@gmail.com>
2022-06-03 03:20:58 +0000

    refactor(client-chat): Improvement of visual aspect in order to better fit with the graphic charter.

--- client/components/Chat.vue
+++ client/components/Chat.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="chat-box" class="w-full h-full flex flex-col">
-		<h1 id="chat-title" class="text-center">Chat</h1>
+		<!--h1 id="chat-title" class="text-center">Chat</h1-->
+		<button id="chat-title" class="btn pr-3 pl-3" @click.prevent="joinRealm">Chat</button>
 		<div id="chat-content" class="flex flex-col-reverse items-center">
 			<Messages />
 		</div>
@@ -9,7 +10,8 @@
 				<textarea
 					id="textarea"
 					v-model="text"
-					class="bg-transparent border-none outline-none resize-none w-full flex-auto"
+					maxlength="1000"
+					class="message-txt bg-transparent border-none outline-none resize-none w-full flex-auto"
 					placeholder="Enter message..."
 					@keydown.enter.prevent="sendMessage"
 				/>
@@ -18,11 +20,11 @@
 				<svg width="20px" height="20px" viewBox="0 0 24 24" class="crt8y2ji">
 					<path
 						d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z"
-						fill="#424242"
+						fill="#d5d5d5"
 					/>
 				</svg>
 			</button>
-			<button id="send" class="btn pr-3 pl-3" @click.prevent="joinRealm">Join..</button>
+			<!--button id="join" class="btn pr-3 pl-3" @click.prevent="joinRealm">Join..</button-->
 		</div>
 	</div>
 </template>
@@ -48,6 +50,7 @@ export default {
 		});
 		this.messages = [];
 		this.name = this.$cookies.get("name");
+		if (!this.name) this.name = "riblanc";
 	},
 	methods: {
 		joinRealm() {
@@ -78,10 +81,14 @@ export default {
 </script>
 
 <style>
+#chat-title {
+	color: #d5d5d5;
+}
+
 .txt-msg-bg {
 	border: none;
-	border-bottom: 1px solid #ccc;
-	background-color: #dadada;
+	border-bottom: 1px solid #333;
+	background-color: #1c2638;
 	border-radius: 15px;
 	outline: none;
 	width: 100%;
@@ -90,8 +97,12 @@ export default {
 	font: 1em "Open Sans", sans-serif;
 }
 
+.message-txt {
+	color: #d5d5d5;
+}
+
 #chat-box {
-	background-color: #f5f5f5;
+	background-color: #1b1b1b;
 }
 
 #chat-content {
