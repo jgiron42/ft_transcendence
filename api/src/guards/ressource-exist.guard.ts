c46e97974416f8bcf34f0b4ba511849a1744c304
joachim <joachim@00101010.fr>
2022-06-08 10:49:38 +0200

    minor fixes

--- api/src/guards/ressource-exist.guard.ts
+++ api/src/guards/ressource-exist.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
+import { Request } from "@src/types/request";
 
 declare type RessourceService = {
 	findOne: (id: string) => object;
@@ -9,9 +10,7 @@ declare type RessourceService = {
 export class RessourceExistGuard<RessourceType extends RessourceService> implements CanActivate {
 	constructor(private ressource: RessourceType) {}
 	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		const request = context.switchToHttp().getRequest();
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
+		const request = context.switchToHttp().getRequest<Request>();
 		if (this.ressource.findOne(request.params.id)) return true;
 		return false;
 	}
--- api/src/utils/requestPipeDecorator.ts
+++ api/src/utils/requestPipeDecorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { Request } from "@src/types/request";
 
-export const RequestPipeDecorator = createParamDecorator((_data: unknown, ctx: ExecutionContext) => {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	return ctx.switchToHttp().getRequest();
+export const RequestPipeDecorator = createParamDecorator((_data: unknown, ctx: ExecutionContext): Request => {
+	return ctx.switchToHttp().getRequest<Request>();
 });
