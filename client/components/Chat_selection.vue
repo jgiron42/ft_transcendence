c903d4a33e38436a2a46cc800740722fa7d09758
frtalleu <frtalleu@student.42.fr>
2022-06-12 17:40:39 +0000

    features(client-chat): begining of the chat selection

--- client/components/Chat_selection.vue
+++ client/components/Chat_selection.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
 	<div id="chat-selection" class="w-1/4 h-full flex flex-col">
 		<button class="btn pr-3 pl-3" @click="$modal.show('create_channel')">Open modal</button>
 		<div v-for="(chan, index) of channels" :key="index">
@@ -20,9 +20,14 @@ export default Vue.extend({
 			type: Object,
 			default: () => {},
 		},
+		// onNewChannel: {
+		//   type: any,
+		//   default: () => {},
+		// },
 	},
 	data() {
 		return {
+			showPopup: false,
 			channels: [] as Channel[],
 		};
 	},
@@ -47,6 +52,69 @@ export default Vue.extend({
 		onGC(chans: Channel[]) {
 			this.channels = chans;
 		},
+		onNewChannel() {
+			this.showPopup = true;
+		},
+		closePopup() {
+			this.showPopup = false;
+		},
 	},
 });
 </script>
+
+<style>
+.chan-text {
+	overflow: hidden;
+	color: #95b5df;
+	font: 1em "Open Sans", sans-serif;
+	width: 100%;
+	padding: 10px;
+	border-radius: 15px;
+	margin-bottom: 10px;
+	text-align: center;
+	background-color: #364157;
+}
+
+.close-button {
+	overflow: hidden;
+	color: black;
+	font: 1em "Open Sans", sans-serif;
+	width: 100%;
+	padding: 10px;
+	border-radius: 15px;
+	margin-bottom: 10px;
+	text-align: center;
+	background-color: white;
+}
+
+.cut-text {
+	text-overflow: ellipsis;
+	overflow: hidden;
+	width: 100%;
+	height: 1.2em;
+	white-space: nowrap;
+}
+
+.newChanPopup {
+	display: none;
+	position: fixed;
+	width: 100%;
+	border: 3px solid #f1f1f1;
+	z-index: 9;
+	color: black;
+	background-color: black;
+}
+
+.showPopup {
+	display: block;
+}
+
+.titlePopup {
+	color: white;
+}
+
+.nobr {
+	display: inline-block;
+	white-space: nowrap;
+}
+</style>
