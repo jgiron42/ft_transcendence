bb2e53e9b312a4bbda47f5cfbb62fced3f773df3
joachim <joachim@00101010.fr>
2022-06-13 12:03:45 +0200

    refactor(API): cleaning modules

--- api/src/modules/chan_connection.module.ts
+++ api/src/modules/chan_connection.module.ts
@@ -1,18 +1,17 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "@src/controllers/app.controller";
-import { UserService } from "@services/user.service";
 import { User } from "@entities/user.entity";
 import { Channel } from "@entities/channel.entity";
-import { ChannelService } from "@services/channel.service";
 import { ChanConnection } from "@entities/chan_connection.entity";
 import { ChanConnectionService } from "@services/chan_connection.service";
+import { UserModule } from "@modules/user.module";
 import { AuthModule } from "./auth.module";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([ChanConnection, User, Channel]), AuthModule],
-	providers: [ChanConnectionService, UserService, ChannelService],
+	imports: [TypeOrmModule.forFeature([ChanConnection, User, Channel]), AuthModule, forwardRef(() => UserModule)],
+	providers: [ChanConnectionService],
 	controllers: [AppController],
-	exports: [ChanConnectionService, UserService, ChannelService],
+	exports: [ChanConnectionService],
 })
 export class ChanConnectionModule {}
--- api/src/modules/message.module.ts
+++ api/src/modules/message.module.ts
@@ -1,18 +1,17 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "@src/controllers/app.controller";
-import { UserService } from "@services/user.service";
 import { User } from "@entities/user.entity";
 import { Channel } from "@entities/channel.entity";
 import { Message } from "@entities/message.entity";
 import { MessageService } from "@services/message.service";
-import { ChannelService } from "@services/channel.service";
+import { UserModule } from "@modules/user.module";
 import { AuthModule } from "./auth.module";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Message, User, Channel]), AuthModule],
-	providers: [MessageService, UserService, ChannelService],
+	imports: [TypeOrmModule.forFeature([Message, User, Channel]), AuthModule, forwardRef(() => UserModule)],
+	providers: [MessageService],
 	controllers: [AppController],
-	exports: [MessageService, UserService, ChannelService],
+	exports: [MessageService],
 })
 export class MessageModule {}
