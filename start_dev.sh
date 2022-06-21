7e32d9b42bc20a7006760dad7354d88f37585528
dirty-no <sonicsupersayen@gmail.com>
2022-06-21 16:08:57 +0000

    feature(easy-launch): Add dev start script

---
+++ start_dev.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Script to easily launch the project in dev mode
+
+cd proxy
+mv templates/default.conf.template ../
+cp dev.default.conf.template templates/
+
+cd ..
+sudo chown -R 5050:5050 pgadmin/
