c2bcddacceddc5f3bbf7637d58194fec5ad105ce
joachim <joachim@00101010.fr>
2022-06-24 00:12:17 +0200

    default value for session secret

--- api/src/config/api.config.ts
+++ api/src/config/api.config.ts
@@ -1,8 +1,11 @@
-export default {
+import * as crypto from "crypto";
+const config = {
 	apiPort: Number(process.env.API_PORT) || 3000,
 	env: process.env.NODE_ENV || "development",
 	cors: { origin: process.env.API_CORS || "*" },
 	sessionTimeout: Number(process.env.API_SESSION_TIMEOUT) || 60000,
-	sessionSecret: process.env.API_SESSION_SECRET.valueOf(),
+	sessionSecret: process.env.API_SESSION_SECRET || crypto.randomBytes(10).toString("hex"),
 	baseUrl: process.env.API_BASE_URL || "http://localhost:3000",
 };
+console.log("sessionSecret:", config.sessionSecret);
+export default config;
