3e8d91454834ae8c20251a6b2c847fbd13d97afb
dirty-no <sonicsupersayen@gmail.com>
2022-06-26 23:59:44 +0000

    style(client-alerts): Add separator between title and msg

--- client/components/Alert.vue
+++ client/components/Alert.vue
@@ -4,7 +4,7 @@
 		class="absolute bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded w-full"
 		role="alert"
 	>
-		<strong class="font-bold">{{ title }}</strong>
+		<strong class="font-bold">{{ title }} | </strong>
 		<span class="block sm:inline">{{ message }}</span>
 		<span class="absolute top-0 bottom-0 right-0 px-4 py-3">
 			<svg
