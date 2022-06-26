73f901c573f4e46d0a4591a895cc31af67838a23
dirty-no <sonicsupersayen@gmail.com>
2022-06-26 23:24:40 +0000

    feature(matchmaking-websocket): Add matchmaking socket init

--- client/components/GameDispatcher.vue
+++ client/components/GameDispatcher.vue
@@ -5,6 +5,7 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { NuxtSocket } from "nuxt-socket-io";
 import Matchmaking from "./Matchmaking.vue";
 import Game from "./Game.vue";
 
@@ -13,8 +14,30 @@ export default Vue.extend({
 	data: () => ({
 		gameID: 42,
 		inGame: false,
+		matchmakingSocket: {} as NuxtSocket,
 	}),
 	mounted() {
+		// Instantiate websocket
+		this.matchmakingSocket = this.$nuxtSocket({
+			name: "matchmaking",
+			channel: "/matchmaking",
+			reconnection: true,
+			reconnectionAttempts: Infinity,
+			reconnectionDelay: 1000,
+			reconnectionDelayMax: 5000,
+			timeout: 10000,
+			autoConnect: true,
+			transports: ["websocket"],
+			teardown: false,
+			forceNew: false,
+		});
+
+		// Log all event
+		this.matchmakingSocket.onAny((event, ...args) => {
+			console.log("[MATCHMAKING]:", event, args);
+		});
+
+		// Handle match found event
 		this.$nuxt.$on("matchFound", (gameID: number) => {
 			// Pass the found gameID to the Game component.
 			this.gameID = gameID;
