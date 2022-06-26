cc3f91ecf0f034771d0d7117621e56ea8cc9ae19
dirty-no <sonicsupersayen@gmail.com>
2022-06-26 20:43:23 +0000

    feature(game-components): Updated index to include GameDispatcher

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,25 +1,41 @@
 <template>
-	<div id="app" class="w-screen h-screen overflow-hidden flex flex-col">
+	<div id="app" class="w-screen overflow-hidden flex flex-col h-full">
 		<Header />
-		<div id="page" class="flex flex-row items-center flex-grow min-h-0 min-w-0 overflow-x-auto">
-			<div class="flex w-1/2 h-1/2">
+		<div id="page" class="flex flex-row items-center content-center justify-evenly overflow-x-auto h-full">
+			<div class="w-fit h-full w-2/5">
 				<Settings />
+			</div>
+			<div>
+				<GameDispatcher />
 			</div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
+import GameDispatcher from "~/components/GameDispatcher.vue";
 export default {
 	name: "Index",
+	components: { GameDispatcher },
 	methods: {},
 };
 </script>
 
 <style>
-body {
+html,
+body,
+#__nuxt,
+#__layout {
 	scroll-behavior: smooth;
 	background-color: bg-design_gray;
+	touch-action: manipulation;
+	-webkit-tap-highlight-color: transparent;
+	height: 100%;
+	width: 100%;
+	margin: 0;
+	padding: 0;
+	overflow-x: hidden;
+
 	@apply bg-design_dgray;
 }
 </style>
