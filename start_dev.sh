eac138ec88384be1dd3f953b2954a5e9dcff0a45
dirty-no <sonicsupersayen@gmail.com>
2022-06-30 13:30:25 +0000

    feature(gitpod-websocket): Add support for websockets and gitpod

--- start_dev.sh
+++ start_dev.sh
@@ -8,4 +8,12 @@ cp dev.default.conf.template templates/default.conf.template
 cd ..
 sudo chown -R 5050:5050 pgadmin/
 
+if ! [ -z "$GITPOD_WORKSPACE_ID" ];then
+    export API_PORT=3000
+    export GITPOD_WORKSPACE_HOST="$GITPOD_WORKSPACE_ID.$GITPOD_WORKSPACE_CLUSTER_HOST"
+    echo "Running on a gitpod instance: $GITPOD_WORKSPACE_HOST"
+    export API_BASE_URL=https://$API_PORT-$GITPOD_WORKSPACE_HOST
+    export WSS_BASE_URL=wss://$API_PORT-$GITPOD_WORKSPACE_HOST
+fi
+
 docker-compose up $@
