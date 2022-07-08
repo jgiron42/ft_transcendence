c20cef38ebecaa2164c37e0d7dd662ee0777778f
frtalleu <frtalleu@student.42.fr>
2022-07-08 23:33:25 +0000

    feature(client-navigation): change header and adding a homepage

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,8 +1,59 @@
 <template>
 	<div>
-		<div class="bg-design_black text-design_white">
-			<Logo />
+		<div class="scroll_menu bg-design_black text-design_white flex flex-row">
+			<button @click.prevent="isActive = !isActive">
+				<Logo />
+			</button>
+			<div v-if="isActive === true" class="scroll_menu flex flex-row">
+				<NuxtLink class="selection_menu" to="/homepage">Homepage</NuxtLink>
+				<NuxtLink class="selection_menu" to="/homepage">Pong</NuxtLink>
+				<NuxtLink class="selection_menu" to="/homepage">Social Network</NuxtLink>
+				<NuxtLink class="selection_menu" to="/homepage">Profile</NuxtLink>
+			</div>
 		</div>
 		<div class="border-design_white border-b border-2"></div>
 	</div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+	data() {
+		return {
+			isActive: false,
+		};
+	},
+});
+</script>
+
+<style>
+.center_elem {
+	position: center;
+	vertical-align: bottom;
+}
+
+.scroll_menu {
+	background-color: design-black;
+	overflow: auto;
+	width: 100%;
+	height: 70px;
+	white-space: nowrap;
+	vertical-align: center;
+}
+
+.selection_menu {
+	width: 100%;
+	height: 100%;
+	margin-right: auto;
+	margin-left: auto;
+	padding: 20px;
+	font: 1em "Open Sans", sans-serif;
+	display: grid;
+	align-content: center;
+	justify-content: center;
+}
+
+.selection_menu:hover {
+	border: 5px solid grey;
+}
+</style>
---
+++ client/pages/homepage.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="app" class="w-screen h-screen overflow-hidden flex flex-col">
+		<Header />
+		<div id="page" class="flex flex-row items-center flex-grow min-h-0 min-w-0 overflow-x-auto">
+			<div class="menu_button flex flex-row">
+				<NuxtLink to="/homepage" class="button_choice">
+					<ProfileLogo />
+				</NuxtLink>
+				<NuxtLink to="/homepage" class="button_choice">
+					<ChatLogo />
+				</NuxtLink>
+				<NuxtLink to="/homepage" class="button_choice">
+					<PongLogo />
+				</NuxtLink>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: "Index",
+	methods: {},
+};
+</script>
+
+<style>
+.menu_button {
+	background-color: design-black;
+	overflow: auto;
+	width: 100%;
+	white-space: nowrap;
+	vertical-align: center;
+}
+
+.button_choice {
+	margin-right: auto;
+	margin-left: auto;
+	display: flex;
+	width: 10%;
+	height: 10%;
+}
+
+body {
+	scroll-behavior: smooth;
+	background-color: bg-design_gray;
+	@apply bg-design_dgray;
+}
+</style>
