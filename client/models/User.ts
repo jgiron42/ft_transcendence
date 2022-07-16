e2c762b2534764e3fd2290932a673f5af9ca6381
dirty-no <sonicsupersayen@gmail.com>
2022-07-16 12:16:36 +0000

    docs(client-user): Added comments for UserSingleton

--- client/models/User.ts
+++ client/models/User.ts
@@ -1,5 +1,6 @@
 import { Context } from "@nuxt/types";
 
+// User type declaration identical to API's one.
 export class User {
 	constructor() {
 		this.id = null;
@@ -27,26 +28,38 @@ export class User {
 // Wrapper of UserStore allowing to automatically fetch, store, save and return the user when needed.
 export class UserSingleton {
 	constructor(ctx: Context) {
+		// Nuxt context allowing you to access the store, runtimeConfig and configured axios.
+		// https://nuxtjs.org/docs/internals-glossary/context/
 		this.ctx = ctx;
 	}
 
 	private ctx: Context;
 
 	private get user(): User {
+		// Clone stored user.
 		return { ...this.ctx.store.getters.getUser };
 	}
 
 	async fetch(): Promise<User> {
 		try {
+			// Get user from API.
 			const response = await this.ctx.$axios.get(this.ctx.$config.ft_api.url + "/me");
 
+			// Ensure returner user is valid.
 			if (response.data.id) {
+				// Store fetched user.
 				this.ctx.store.commit("setUser", response.data);
+
+				// Return fetched user.
 				return response.data;
 			}
+
+			// Return stored user in case of bad response.
 			return this.user;
 		} catch (err: any) {
+			// Handle missing auth.
 			if (err.response && err.response.data.authMethod) {
+				// Only set user id when 42 auth is missing.
 				if (err.response.data.authMethod === "42") {
 					const user = this.user;
 					user.id = err.response.data.id;
@@ -54,26 +67,39 @@ export class UserSingleton {
 					this.setUser(user);
 					return this.user;
 				}
+				// Handle missing TOTP auth.
 				if (err.response.data.authMethod === "TOTP") {
+					// Redirect to 2FA component.
 					this.ctx.redirect("/totp_authenticate");
 				}
 			}
+
+			// Throw caught error again
 			throw err;
 		}
 	}
 
 	async getUser(): Promise<User> {
+		// Get stored user
 		const storedUser = this.user;
+
+		// Ensure stored user is valid, else fetch and return one from API
 		if (storedUser.id === null) return await this.fetch();
+
+		// Return stored user
 		return this.user;
 	}
 
 	async save() {
+		// Get stored user
 		const user = this.user;
+
+		// Save stored user in database.
 		await this.ctx.$axios.$put(`/users/${user.id}`, user);
 	}
 
 	setUser(user: User) {
+		// Replace stored user with parameter
 		this.ctx.store.commit("setUser", user);
 	}
 }
